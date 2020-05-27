eaf0e55d55ac6fca7bb2eaf36479cec21be3a7f0
Hobby <hejiandong@chatly.com>
2020-05-27 15:40:41 +0800

    update doc

--- env/dev.env.js
+++ env/dev.env.js
@@ -2,5 +2,5 @@
 
 module.exports = {
   APP_ENV: 'development',
-  BASE_URL: 'https://cds-dev.wechatify.net/api/tmg_addon/wxapp_cds'
+  BASE_URL: 'https://**'
 }
--- env/prod.env.js
+++ env/prod.env.js
@@ -2,5 +2,5 @@
 
 module.exports = {
   APP_ENV: 'production',
-  BASE_URL: 'https://tda-staging.wechatify.net/api/lms/v2/mp'
+  BASE_URL: 'https://**'
 }
